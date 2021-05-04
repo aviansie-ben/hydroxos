1249a8c2e0b2b772741971395d215e7852b011f6
Benjamin <ben@benthomas.ca>
2021-05-04 12:48:49 -0400

    Initialize x87 and SSE support during boot

--- kernel/src/x86_64/mod.rs
+++ kernel/src/x86_64/mod.rs
@@ -15,6 +15,14 @@ pub unsafe fn create_primary_display(_: &'static BootInfo) -> VirtualTerminalDis
     VirtualTerminalDisplay::VgaText(TextBuffer::new(0xb8000 as *mut u8, 80, 25))
 }
 
+unsafe fn init_sse() {
+    use x86_64::registers::control::{Cr0, Cr0Flags, Cr4, Cr4Flags};
+
+    Cr0::write(Cr0::read() & !Cr0Flags::EMULATE_COPROCESSOR | Cr0Flags::MONITOR_COPROCESSOR);
+    Cr4::write(Cr4::read() | Cr4Flags::OSFXSR | Cr4Flags::OSXMMEXCPT_ENABLE);
+    asm!("fninit");
+}
+
 pub unsafe fn init_phase_1(boot_info: &'static BootInfo) {
     use x86_64::instructions::interrupts;
 
@@ -25,4 +33,6 @@ pub unsafe fn init_phase_1(boot_info: &'static BootInfo) {
     pic::remap_pic(idt::IRQS_START, idt::IRQS_START + 0x8);
     pic::mask_all_irqs();
     interrupts::enable();
+
+    init_sse();
 }
