25d909e21a1be27cc07e23fd966240c9ecaff86e
Benjamin <ben@benthomas.ca>
2022-02-24 23:38:52 -0500

    Implement basic early de- and reallocation
    Previously, the early allocator used a very naive bump pointer
    allocation method. While this was fine before, the use of format! and
    futures in logging means that a number of temporary object can get
    allocated, reallocated, and deallocated in rapid succession.
    
    As a minor optimization, the early allocator can now handle shrinking
    allocations in place without needing to allocate new memory and can also
    handle growing and freeing the last allocation made in-place.

--- kernel/src/early_alloc.rs
+++ kernel/src/early_alloc.rs
@@ -1,5 +1,6 @@
 use core::alloc::{GlobalAlloc, Layout};
 use core::convert::TryFrom;
+use core::ptr;
 use core::sync::atomic::{AtomicPtr, Ordering};
 
 use crate::util::{PageAligned, SharedUnsafeCell};
@@ -24,22 +25,36 @@ pub fn init() {
     };
 }
 
-pub fn alloc(size: usize, align: usize) -> *mut u8 {
-    unsafe {
-        if size.checked_add(align).and_then(|sz| isize::try_from(sz).ok()).is_none() {
-            panic!("Max allocation size too large");
-        };
+fn get_full_size(size: usize) -> u32 {
+    if size == 0 {
+        4
+    } else {
+        u32::try_from(size)
+            .ok()
+            .and_then(|sz| ((sz - 1) & !3).checked_add(8))
+            .expect("Early allocation too large")
+    }
+}
 
-        let alloc_area = &*EARLY_ALLOC_AREA.get();
+pub fn alloc(size: usize, align: usize) -> *mut u8 {
+    // We always need at least 4 byte alignment, since we store the 4 byte allocation size after each block
+    let align = u32::try_from(align.max(4)).ok().expect("Early allocation too large");
+    let size = get_full_size(size);
+
+    unsafe {
+        let early_alloc_end = (*EARLY_ALLOC_AREA.get()).as_mut_ptr_range().end;
 
         loop {
             let mark = EARLY_ALLOC_MARK.load(Ordering::Relaxed);
-            let align_offset = mark.align_offset(align);
-            let alloc_size = (size + align_offset) as isize;
+            let align_offset = mark.align_offset(align as usize) as u32;
+            let alloc_size = size
+                .checked_add(align_offset)
+                .and_then(|sz| isize::try_from(sz).ok())
+                .expect("Early allocation too large");
 
             if mark.is_null() {
                 panic!("Attempt to use early memory allocation before initializing it");
-            } else if alloc_area.as_ptr_range().end.offset_from(mark) < alloc_size {
+            } else if early_alloc_end.offset_from(mark) < alloc_size {
                 panic!("Out of early allocation memory");
             };
 
@@ -47,14 +62,81 @@ pub fn alloc(size: usize, align: usize) -> *mut u8 {
                 .compare_exchange(mark, mark.offset(alloc_size), Ordering::Relaxed, Ordering::Relaxed)
                 .is_ok()
             {
-                break mark.add(align_offset);
+                *(mark.add((align_offset + size - 4) as usize) as *mut u32) = alloc_size as u32;
+                break mark.add(align_offset as usize);
             };
         }
     }
 }
 
-pub fn free(_: *mut u8) {
-    // TODO Implement once we move away from bump allocation
+pub unsafe fn free(ptr: *mut u8, size: usize) {
+    let size = get_full_size(size);
+    let mark = EARLY_ALLOC_MARK.load(Ordering::Relaxed);
+
+    if mark == ptr.add(size as usize) {
+        let real_size = *(ptr.add((size - 4) as usize) as *mut u32) as usize;
+
+        let _ = EARLY_ALLOC_MARK.compare_exchange(mark, mark.sub(real_size), Ordering::Relaxed, Ordering::Relaxed);
+    }
+}
+
+unsafe fn realloc_grow(ptr: *mut u8, old_size: usize, new_size: usize) -> *mut u8 {
+    let old_size = get_full_size(old_size);
+    let new_size = get_full_size(new_size);
+    let mark = EARLY_ALLOC_MARK.load(Ordering::Relaxed);
+
+    if mark == ptr.add(old_size as usize) {
+        if (*EARLY_ALLOC_AREA.get()).as_mut_ptr_range().end.offset_from(mark)
+            < isize::try_from(new_size - old_size).expect("Early allocation too large")
+        {
+            panic!("Out of early allocation memory");
+        }
+
+        let real_old_size = *(ptr.add((old_size - 4) as usize) as *mut u32);
+        let real_new_size = (real_old_size - old_size)
+            .checked_add(new_size)
+            .expect("Early allocation too large");
+
+        if EARLY_ALLOC_MARK
+            .compare_exchange(mark, ptr.add(new_size as usize), Ordering::Relaxed, Ordering::Relaxed)
+            .is_ok()
+        {
+            *(ptr.add((new_size - 4) as usize) as *mut u32) = real_new_size;
+            return ptr;
+        }
+    }
+
+    ptr::null_mut()
+}
+
+unsafe fn realloc_shrink(ptr: *mut u8, old_size: usize, new_size: usize) -> *mut u8 {
+    let old_size = get_full_size(old_size);
+    let new_size = get_full_size(new_size);
+    let mark = EARLY_ALLOC_MARK.load(Ordering::Relaxed);
+
+    if mark == ptr.add(old_size as usize) {
+        let real_old_size = *(ptr.add((old_size - 4) as usize) as *mut u32);
+        let real_new_size = real_old_size - old_size + new_size;
+
+        if EARLY_ALLOC_MARK
+            .compare_exchange(mark, ptr.add(new_size as usize), Ordering::Relaxed, Ordering::Relaxed)
+            .is_ok()
+        {
+            *(ptr.add((new_size - 4) as usize) as *mut u32) = real_new_size;
+        }
+    }
+
+    ptr
+}
+
+pub unsafe fn realloc(ptr: *mut u8, old_size: usize, new_size: usize) -> *mut u8 {
+    if new_size > old_size {
+        realloc_grow(ptr, old_size, new_size)
+    } else if new_size < old_size {
+        realloc_shrink(ptr, old_size, new_size)
+    } else {
+        ptr
+    }
 }
 
 struct EarlyAlloc;
@@ -64,8 +146,23 @@ unsafe impl GlobalAlloc for EarlyAlloc {
         alloc(layout.size(), layout.align())
     }
 
-    unsafe fn dealloc(&self, ptr: *mut u8, _: Layout) {
-        free(ptr);
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+        free(ptr, layout.size());
+    }
+
+    unsafe fn realloc(&self, ptr: *mut u8, layout: Layout, new_size: usize) -> *mut u8 {
+        let new_ptr = realloc(ptr, layout.size(), new_size);
+
+        if !new_ptr.is_null() {
+            new_ptr
+        } else {
+            let new_ptr = self.alloc(Layout::from_size_align_unchecked(new_size, layout.align()));
+
+            ptr::copy_nonoverlapping(ptr, new_ptr, layout.size().min(new_size));
+            self.dealloc(ptr, layout);
+
+            new_ptr
+        }
     }
 }
 
